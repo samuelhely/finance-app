69aed22cd0d86160253b61824cb933fc85befd53
Samuel Hely <samuel_hely@hotmail.com>
2026-04-12 20:47:55 -0300

    update: added casts to Transaction model

--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -2,6 +2,10 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionPaymentMethod;
+use App\Enums\TransactionSourceType;
+use App\Enums\TransactionStatus;
+use App\Enums\TransactionType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -24,7 +28,11 @@ class Transaction extends Model
     ];
 
     protected $casts = [
-        'date' => 'date'
+        'date' => 'date',
+        'type' => TransactionType::class,
+        'payment_method' => TransactionPaymentMethod::class,
+        'ocurrence_status' => TransactionStatus::class,
+        'source_type' => TransactionSourceType::class,
     ];
 
     public function user(): BelongsTo
